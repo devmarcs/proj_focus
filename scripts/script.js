a75085e21e305bab130fd4ae44c24db1a12a46ba
devmarcs <devmarcelo.gus@gmail.com>
2023-11-24 15:20:56 -0300

    ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,9 @@ const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 
-focoBt.addEventListener('click', () =>{
+/*focoBt.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'foco');
+    console.log(focoBt);
 })
 
 curtoBt.addEventListener('click', () => {
@@ -13,24 +14,27 @@ curtoBt.addEventListener('click', () => {
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
-})
+})*/
 
 
 
 
 
 
-/*const botoes = document.querySelectorAll('.app__card-button');
+const botoes = document.querySelectorAll('.app__card-button');
 console.log(botoes);
 
 botoes.forEach((botao) => {
     botao.addEventListener('click', (e) => {
-        click(e)
+        console.log(botao.getAtribute('data-contexto'));
+        botao.currentTarget.getAtribute('data-contexto')
     });
 })
 
-function click (e) {
-    e.content.addClassName('active')
+function click () {
+    
+    
+    console.log('saiu aqui');
 }
 
 const botaoSelect = () => {
@@ -44,5 +48,5 @@ for (let i = 0; i < botoes.length; i += 1) {
         html.setAttribute('data-contexto', 'foco')
    })
 
-}*/
+}
 
