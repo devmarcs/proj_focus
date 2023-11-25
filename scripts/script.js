109700b34430fdd354c65207ea8ded565f56b101
devmarcs <devmarcelo.gus@gmail.com>
2023-11-25 03:02:07 -0300

    ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,29 @@ const html = document.querySelector('html');
 const focoBt = document.querySelector('.app__card-button--foco');
 const curtoBt = document.querySelector('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
+const banner = document.querySelector('.app__image')
 
-/*focoBt.addEventListener('click', () =>{
+
+focoBt.addEventListener('click', () =>{
     html.setAttribute('data-contexto', 'foco');
-    console.log(focoBt);
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto');
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo');
-})*/
+})
 
 
 
 
 
 
-const botoes = document.querySelectorAll('.app__card-button');
+/*const botoes = document.querySelectorAll('.app__card-button');
 console.log(botoes);
 
 botoes.forEach((botao) => {
@@ -48,5 +51,5 @@ for (let i = 0; i < botoes.length; i += 1) {
         html.setAttribute('data-contexto', 'foco')
    })
 
-}
+}*/
 
